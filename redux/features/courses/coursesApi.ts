f6650f8724aac478ba77d3ad8f9e776371a166d6
Mevin5611 <mevinmevi88@gmail.com>
2024-03-18 23:55:21 +0530

    change url of some api

--- redux/features/courses/coursesApi.ts
+++ redux/features/courses/coursesApi.ts
@@ -46,6 +46,13 @@ export const courseApi = apiSlice.injectEndpoints({
         credentials: "include" as const,
       }),
     }),
+    getCourseContnet: builder.query({
+      query: (id) => ({
+        url: `/getcourseby-user/${id}`,
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
   }),
 });
 
@@ -56,4 +63,5 @@ export const {
   useEditCourseMutation,
   useGetAllCoursesForUsersQuery,
   useGetCourseDetailsQuery,
+  useGetCourseContnetQuery,
 } = courseApi;
