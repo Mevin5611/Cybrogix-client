bc4362ca10c2e8df80d5eb525a70dbe4bc2ab89b
Mevin5611 <mevinmevi88@gmail.com>
2024-03-19 23:36:08 +0530

    corsecontent media page added

--- app/components/course/CourseContentMedia.tsx
+++ app/components/course/CourseContentMedia.tsx
@@ -1,14 +1,19 @@
 import { useGetCourseContnetQuery } from '@/redux/features/courses/coursesApi'
-import React from 'react'
+import React, { useState } from 'react'
 import Loader from '../Loader/Loader'
+import Heading from '@/app/utils/Heading'
+import CourseMedia from './CourseMedia'
+import CourseContentList from './CourseContentList'
 
 type Props = {
     id:string
+    user:any
 }
 
-const CourseContentMedia = ({id}: Props) => {
+const CourseContentMedia = ({id,user}: Props) => {
     const {data,isLoading} = useGetCourseContnetQuery(id)
     console.log("content",data);
+    const [activeVideo,setActiveVideo] = useState(0)
     
   return (
     <>
@@ -17,7 +22,26 @@ const CourseContentMedia = ({id}: Props) => {
             <Loader/>
         ) : (
             <div>
-                
+                <Heading 
+                title={data?.content[activeVideo]?.title}
+                description="anything"
+                keyword={data?.content[activeVideo]?.tags}
+                />
+            <div className='w-full grid 800px:grid-cols-10'>
+                <div className="col-span-7">
+                    <CourseMedia 
+                    data={data}
+                    id={id}
+                    activeVideo={activeVideo}
+                    setActiveVideo={setActiveVideo}
+                    userData={user}
+                    />
+                </div>
+                <div className='hidden 800px:block 800px:col-span-3 '>
+                <CourseContentList data={data?.content} isDemo={false} activeVideo={activeVideo}
+                    setActiveVideo={setActiveVideo}/>
+                </div>
+            </div>
             </div>
         )
     }
