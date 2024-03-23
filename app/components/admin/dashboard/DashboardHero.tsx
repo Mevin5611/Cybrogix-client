8bd1a95ff752588ed4f0d5b94837fac45b69c634
Mevin5611 <mevinmevi88@gmail.com>
2024-03-23 23:39:30 +0530

    something changed

--- app/components/admin/dashboard/DashboardHero.tsx
+++ app/components/admin/dashboard/DashboardHero.tsx
@@ -2,13 +2,17 @@ import React, { FC, useState } from 'react'
 import DashboardHeader from './DashboardHeader'
 import DashboardWidget from '../widgets/DashboardWidget'
 
+
 type Props = {
   isDashboard:boolean
 }
 
 const DashboardHero:FC<Props> = (isDashboard) => {
+  
   const [open,setOpen] = useState(false)
   const [value,setValue] = useState(0)
+
+  
   return (
     <div>
         <DashboardHeader/>
