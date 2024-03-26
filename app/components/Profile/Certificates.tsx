7514cfa4c0dfe972df32a7f30c6b282cac5d1774
Mevin5611 <mevinmevi88@gmail.com>
2024-03-27 01:41:15 +0530

    certificates page

---
+++ app/components/Profile/Certificates.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Certificate from './Certificate';
+
+type Props = {
+    user:any
+}
+
+const Certificates = ({user}: Props) => {
+    console.log(user);
+    
+  return (
+    <div>
+        <div className='ps-5'>
+            <h1 className='font-Poppins text-[30px] font-[600]'>{user.name}</h1>
+
+            <h1 className='pt-4 font-Poppins text-[18px]'>Download your certificate in here</h1>
+
+            <div className='grid grid-cols-4'>
+            {
+                user && user.certificates
+                .map((item:any,index:number)=>(
+
+                    <Certificate key={index} item={item}/>
+
+                ))
+            }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Certificates
